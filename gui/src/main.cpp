1e3168b80127cc7427aafaab091bd8a788808d31
Louis <louis.bagnol@epitech.eu>
2026-05-30 01:05:55 +0200

    feat: replace WorldState with GameState in main function for improved event handling

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,4 +1,5 @@
-#include "core/WorldState.hpp"
+#include "core/GameState.hpp"
+#include "core/Event.hpp"
 #include "renderer/HeadlessRenderer.hpp"
 #include <iostream>
 
@@ -7,9 +8,17 @@ int main(int argc, char** argv)
     (void)argc;
     (void)argv;
 
-    WorldState state;
-    state.width = 10;
-    state.height = 10;
+    GameState state;
+    state.applyEvent(MapSize{10, 10});
+    state.applyEvent(TeamName{"Red"});
+    state.applyEvent(TeamName{"Blue"});
+    state.applyEvent(PlayerNew{1, 2, 3, Orientation::N, 1, "Red"});
+    state.applyEvent(PlayerNew{2, 5, 6, Orientation::E, 1, "Blue"});
+    state.applyEvent(PlayerNew{3, 7, 8, Orientation::S, 1, "Red"});
+    state.applyEvent(PlayerPosition{1, 6, 7, Orientation::W});
+    state.applyEvent(TimeUnit{42});
+    state.applyEvent(EggNew{1, 1, 4, 4});
+    state.applyEvent(GameEnd{"Red"});
 
     HeadlessRenderer renderer(std::cout);
     renderer.init();
