31e312a7d9a5bbfd05497a6a2e2bfde4e7896add
Louis <louis.bagnol@epitech.eu>
2026-05-30 01:10:31 +0200

    feat: add PlayerLevel event to GameState in main function

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv)
     state.applyEvent(PlayerNew{2, 5, 6, Orientation::E, 1, "Blue"});
     state.applyEvent(PlayerNew{3, 7, 8, Orientation::S, 1, "Red"});
     state.applyEvent(PlayerPosition{1, 6, 7, Orientation::W});
+    state.applyEvent(PlayerLevel{1, 2});
     state.applyEvent(TimeUnit{42});
     state.applyEvent(EggNew{1, 1, 4, 4});
     state.applyEvent(GameEnd{"Red"});
