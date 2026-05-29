80abd6d3b179e8a80341653ff3a4a1fddf053501
Louis <louis.bagnol@epitech.eu>
2026-05-29 17:19:18 +0200

    feat: define cardinal directions in Orientation enum with explicit values

--- gui/src/core/Orientation.hpp
+++ gui/src/core/Orientation.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+/**
+ * @brief Represents the orientation of an object in the game.
+ * Orientation mapped to the intergers as required by the protocol.
+ */
 enum class Orientation {
-    N,
-    E,
-    S,
-    W
+    N = 1,
+    E = 2,
+    S = 3,
+    W = 4
 };
