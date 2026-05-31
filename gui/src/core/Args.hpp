6eb262821b5a546cffdc9a5cb58d0b9c32b1baae
Louis <louis.bagnol@epitech.eu>
2026-05-31 15:40:39 +0200

    feat: add detailed documentation for Args class methods

--- gui/src/core/Args.hpp
+++ gui/src/core/Args.hpp
@@ -12,9 +12,28 @@ class Args {
 public:
     Args(int argc, char** argv);
 
+    /**
+     * @brief Checks if the parsed arguments are valid and if the application should run.
+     * @return true if the arguments are valid and the application should run, false otherwise.
+     */
     bool isValid() const;
+
+    /**
+     * @brief Checks if the help flag was provided in the arguments.
+     * @return true if the help flag was provided, false otherwise.
+     */
     bool isHelpRequested() const;
+
+    /**
+     * @brief Gets the exit code for the application.
+     * @return The exit code.
+     */
     int exitCode() const;
+
+    /**
+     * @brief Gets the configuration for the application.
+     * @return The configuration.
+     */
     AppConfig getConfig() const;
 
 private:
