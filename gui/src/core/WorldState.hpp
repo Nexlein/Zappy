6894b6556428cca446748bbcd35c1ef43a0ce69c
Louis <louis.bagnol@epitech.eu>
2026-06-12 11:34:40 +0200

    feat: add VisualState class for managing entity visual behaviors

--- gui/src/core/WorldState.hpp
+++ gui/src/core/WorldState.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <memory>
 #include <ostream>
 #include <string>
 #include <unordered_map>
@@ -7,6 +8,31 @@
 
 #include "Orientation.hpp"
 #include "Resources.hpp"
+#include "behaviors/IBehavior.hpp"
+#include "raylib.h"
+
+/**
+ * @brief Visual-only state for an entity, driven by behaviors each frame.
+ * Logical state (x, y, orientation) remains authoritative from the server.
+ */
+class VisualState {
+    public:
+    mutable Vector3 pos = {0.0f, 0.0f, 0.0f};
+    mutable float angle = 0.0f;
+    mutable std::vector<std::unique_ptr<IBehavior>> behaviors;
+
+    void update(float dt) const
+    {
+        for (auto& b : behaviors)
+            b->update(dt);
+        for (auto it = behaviors.begin(); it != behaviors.end();) {
+            if ((*it)->isDone())
+                it = behaviors.erase(it);
+            else
+                ++it;
+        }
+    }
+};
 
 /**
  * @brief Represents a player in the game.
@@ -20,6 +46,7 @@ struct Player {
     std::string team;
     Resources inventory = {};
     bool incanting = false;
+    VisualState visual;
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Player& player)
