34a9efd459892db5c774e82aa285341dc97139e2
Louis <louis.bagnol@epitech.eu>
2026-06-15 17:10:27 +0200

    feat(gui): implement particle spawning for death animation and update position with gravity

--- gui/src/core/behaviors/DeathBehavior.hpp
+++ gui/src/core/behaviors/DeathBehavior.hpp
@@ -14,7 +14,10 @@ class DeathBehavior : public IBehavior {
     bool isDone() const override;
 
     private:
+    void _spawnParticles();
+
     VisualState& _visual;
     float _duration;  // in seconds, 10 ticks / server_tick_rate
     float _elapsed = 0.0f;
+    bool _spawned = false;
 };
