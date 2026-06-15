6843cbd8de8d8e0b41e9ad980b089e83d1701fc1
Louis <louis.bagnol@epitech.eu>
2026-06-15 16:42:57 +0200

    feat(gui): update DeathBehavior to use fixed duration for animation scaling temporarily

--- gui/src/core/behaviors/DeathBehavior.cpp
+++ gui/src/core/behaviors/DeathBehavior.cpp
@@ -1,7 +1,5 @@
 #include "DeathBehavior.hpp"
 
-#include <iostream>
-
 DeathBehavior::DeathBehavior(VisualState& visual, float server_tick_rate)
     : _visual(visual), _duration(10.0f / server_tick_rate)
 {
@@ -10,11 +8,11 @@ DeathBehavior::DeathBehavior(VisualState& visual, float server_tick_rate)
 void DeathBehavior::update(float dt)
 {
     _elapsed += dt;
-    float t = _elapsed / _duration;
+    float t = _elapsed / 1.0f;  // TODO: use tick-based duration
     if (t > 1.0f) t = 1.0f;
 
-    // temp
-    std::cout << "[DeathBehavior] t: " << t << std::endl;
+    float smooth = t * t * (3.0f - 2.0f * t);
+    _visual.scale = 1.0f - smooth * (1.0f - 0.05f);
 }
 
-bool DeathBehavior::isDone() const { return _elapsed >= _duration; }
+bool DeathBehavior::isDone() const { return _elapsed >= 1.0f; }  // TODO: use tick-based duration
