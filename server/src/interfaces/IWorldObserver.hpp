043f1712a705458083b31f8808452b7492ff536f
Lucas Pujol <lucas.pujol@epitech.eu>
2026-06-17 22:30:42 +0200

    feat(IWorldObserver): add interface for world observer events

---
+++ server/src/interfaces/IWorldObserver.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "core/data/Orientation.hpp"
+#include "core/data/Resources.hpp"
+
+class IWorldObserver {
+    public:
+    virtual ~IWorldObserver() = default;
+
+    virtual void onPlayerAdded(int playerId, int x, int y, Orientation orientation, int level,
+                               const std::string& teamName) = 0;
+    virtual void onPlayerMoved(int playerId, int nx, int ny, Orientation newOrientation) = 0;
+    virtual void onPlayerRemoved(int playerId) = 0;
+    virtual void onPlayerEjected(int playerId) = 0;
+    virtual void onBroadcast(int playerId, const std::string& message) = 0;
+    virtual void onResourceTaken(int playerId, ResourceType resourceType, int tileX, int tileY,
+                                 Resources resources) = 0;
+    virtual void onResourceDropped(int playerId, ResourceType resourceType, int tileX, int tileY,
+                                   Resources resources) = 0;
+    virtual void onEggLaid(int eggId, int playerId, int x, int y) = 0;
+    virtual void onEggHatched(int eggId) = 0;
+    virtual void onIncantationStart(int x, int y, const std::vector<int>& participantIds) = 0;
+    virtual void onIncantationEnd(int x, int y, bool success) = 0;
+    virtual void onPlayerLevelUp(int playerId, int newLevel) = 0;
+    virtual void onGameEnd(const std::string& winningTeam) = 0;
+    virtual void onTileChanged(int x, int y, Resources resources) = 0;
+};
