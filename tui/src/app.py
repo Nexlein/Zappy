bf21d0a4ada92aa9aa3f302fedaca82cee124fc2
Lucas Pujol <lucas.pujol@epitech.eu>
2026-06-22 17:52:10 +0200

    lint(tui): formatted using `uvx ruff format`

--- tui/src/app.py
+++ tui/src/app.py
@@ -1,6 +1,7 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Static
 
+
 class ZappyTUI(App):
     TITLE = "Zappy orchestrator"
 
@@ -8,4 +9,3 @@ class ZappyTUI(App):
         yield Header()
         yield Static("zappy tui scaffold")
         yield Footer()
-
--- tui/src/main.py
+++ tui/src/main.py
@@ -1,8 +1,10 @@
 from app import ZappyTUI
 
+
 def main():
     app = ZappyTUI()
     app.run()
+
 
 if __name__ == "__main__":
     main()
