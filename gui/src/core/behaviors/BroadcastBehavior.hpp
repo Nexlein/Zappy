45039d0f755db95cbd222b854bd65fac8f312a32
Louis <louis.bagnol@epitech.eu>
2026-06-26 10:19:14 +0200

    feat(behavior): set minimum duration to 0.0f for Broadcast, Death, and LevelUp behaviors

--- gui/src/core/behaviors/BroadcastBehavior.hpp
+++ gui/src/core/behaviors/BroadcastBehavior.hpp
@@ -19,7 +19,7 @@ class BroadcastBehavior : public ADrawableBehavior {
     void update(float dt) override;
     bool isDone() const override;
     float getDuration() const override { return _duration; }
-    float minDuration() const override { return 0.8f; }
+    float minDuration() const override { return 0.0f; }
 
     private:
     struct ScatterSeed {
